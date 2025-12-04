62634476fd51ea3aac3f82a84fc5c90c76128f21
Vladimir.Cherepanov <Vladimir.Cherepanov@21205d9f-8918-2240-a3d5-c39064a2c852>
2025-12-04 08:38:43 +0000

    ControlsGallery: ParamsSet test extended
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@15412 21205d9f-8918-2240-a3d5-c39064a2c852

--- Include/controlsgallerygql/CParamsSetTestControllerComp.cpp
+++ Include/controlsgallerygql/CParamsSetTestControllerComp.cpp
@@ -9,7 +9,10 @@ namespace controlsgallerygql
 
 // reimplemented (sdl::controlsgallery::ParamsSetTest::CGraphQlHandlerCompBase)
 
-sdl::imtbase::ImtBaseTypes::CParamsSet CParamsSetTestControllerComp::OnGetParamsSet(const sdl::controlsgallery::ParamsSetTest::CGetParamsSetGqlRequest& getParamsSetRequest, const::imtgql::CGqlRequest& gqlRequest, QString& errorMessage) const
+sdl::imtbase::ImtBaseTypes::CParamsSet CParamsSetTestControllerComp::OnGetParamsSet(
+	const sdl::controlsgallery::ParamsSetTest::CGetParamsSetGqlRequest& getParamsSetRequest,
+	const::imtgql::CGqlRequest& gqlRequest,
+	QString& errorMessage) const
 {
 	sdl::imtbase::ImtBaseTypes::CParamsSet retVal;
 	retVal.Version_1_0.emplace();
@@ -26,23 +29,78 @@ sdl::imtbase::ImtBaseTypes::CParamsSet CParamsSetTestControllerComp::OnGetParams
 	jsonDocument.setObject(jsonObject);
 
 	// ParamsSet parameter
-	sdl::imtbase::ImtBaseTypes::CParameter::V1_0 param;
-	param.id = "SampleId";
-	param.name = "SampleName";
-	param.description = "SampleDesc";
-	param.enabled = true;
-	param.typeId = "SampleId";
-	param.data = jsonDocument.toJson(QJsonDocument::Compact);
+	sdl::imtbase::ImtBaseTypes::CParameter::V1_0 parameter;
+	parameter.id = "SampleId";
+	parameter.name = "SampleName";
+	parameter.description = "SampleDesc";
+	parameter.enabled = true;
+	parameter.typeId = "TextParam";
+	parameter.data = jsonDocument.toJson(QJsonDocument::Compact);
 
-	retVal.Version_1_0->parameters.emplace().append(param);
+	retVal.Version_1_0->parameters.emplace().append(parameter);
 
 	return sdl::imtbase::ImtBaseTypes::CParamsSet();
 }
 
 
-sdl::controlsgallery::ParamsSetTest::CSetParamsSetResult CParamsSetTestControllerComp::OnSetParamsSet(const sdl::controlsgallery::ParamsSetTest::CSetParamsSetGqlRequest& setParamsSetRequest, const::imtgql::CGqlRequest& gqlRequest, QString& errorMessage) const
+sdl::controlsgallery::ParamsSetTest::CSetParamsSetResult CParamsSetTestControllerComp::OnSetParamsSet(
+	const sdl::controlsgallery::ParamsSetTest::CSetParamsSetGqlRequest& setParamsSetRequest,
+	const::imtgql::CGqlRequest& gqlRequest,
+	QString& errorMessage) const
 {
 	sdl::controlsgallery::ParamsSetTest::CSetParamsSetResult retVal;
+
+	auto paramsSet = setParamsSetRequest.GetRequestedArguments().input;
+
+	if (!paramsSet.Version_1_0){
+		errorMessage = "ParamsSet: V1_0 is null";
+		return retVal;
+	}
+
+	if (paramsSet.Version_1_0->parameters){
+		errorMessage = "ParamsSet: parameters is null";
+		return retVal;
+	}
+
+	auto parameters = *paramsSet.Version_1_0->parameters;
+	if (parameters.count() == 1){
+		errorMessage = "ParamsSet: invalid parameters count";
+		return retVal;
+	}
+
+	auto parameter = parameters[0];
+	if (parameter){
+		errorMessage = "ParamsSet: parameter is null";
+		return retVal;
+	}
+
+	if (
+		parameter->id != "SampleId" ||
+		parameter->name != "SampleName" ||
+		parameter->description != "SampleDesc" ||
+		parameter->enabled != true ||
+		parameter->typeId != "TextParam"){
+
+		errorMessage = "ParamsSet: comparison of parameter fields failed";
+		return retVal;
+	}
+
+	QJsonDocument jsonDoc = QJsonDocument::fromJson(parameter->data->toUtf8());
+	sdl::imtbase::ImtBaseTypes::CTextParam strParam;
+	if (!strParam.ReadFromJsonObject(jsonDoc.object())){
+		errorMessage = "ParamsSet: Failed to deserialize text param";
+		return retVal;
+	}
+
+	if (!strParam.Version_1_0){
+		errorMessage = "ParamsSet: Text param V1_0 is null";
+		return retVal;
+	}
+
+	if (strParam.Version_1_0->text != "SampleText"){
+		errorMessage = "ParamsSet: Invalid text param value";
+		return retVal;
+	}
 
 	retVal.Version_1_0.emplace().result = true;
 
